142aa3159b45a8548856c7635fbf2f0c839bb49a
tomas juarez <mokajua@gmail.com>
2022-03-18 13:21:59 -0300

    estilos cart

--- my-ecommerce/src/components/Cart/Cart.js
+++ my-ecommerce/src/components/Cart/Cart.js
@@ -46,54 +46,88 @@ export const Cart = () =>{
         },500)
     } 
 
-
-  
-
-  
 return (
     <>
-        {orderRef?(<div className='container'><h1>Su compra ha sido procesada.</h1><h2> codigo de seguimiento:{orderRef}</h2></div>) : ( <div className='container'> 
+        {orderRef?(<div className='container alert-success border border-success rounded cartSucces'><h1>Su compra ha sido procesada.</h1><h2> codigo de seguimiento:{orderRef}</h2></div>) : ( 
+        <div className='container'> 
             <div className='row cartController'>
-                {cartProducts.length === 0 ? (<h2 className='cartEmpty'>No hay productos agregados! <Link to={"/"}>
-                    
-                    <button className='btn btn-success'>Seguir comprando!</button></Link> </h2> ) 
-                    
-                    :(cartProducts.map((prod)=>{
-                    return(
-                       <div className='col-md-3'>
-                            <Card style={{ width: '18rem'}}>
-                                <Card.Img className='CartImg' variant="top" src={prod.Img} style={{ height: '300px'}} />
-                                <Card.Body>
-                                    <Card.Title>{prod.Producto}</Card.Title>
-                                </Card.Body>
-                                <ListGroup className="list-group-flush">
-                                    <ListGroupItem>{prod.quantity}</ListGroupItem>
-                                    <ListGroupItem>Precio por unidad: ${prod.Precio}</ListGroupItem>
-                                    <ListGroupItem>Precio por cantidad: ${prod.quantity * prod.Precio}</ListGroupItem>
-                                </ListGroup>
+                <div className='col-md-8'>
+                    <div className='container'>
+                        <div className='row'>
+                            {cartProducts.length === 0 ? (<h2 className='cartEmpty'>No hay productos agregados! <Link to={"/"}>
                             
-                                <button onClick={() =>removeItems(prod)}>eliminar producto</button>
-                            </Card>
-                        </div>)
+                            <button className='btn btn-success'>Seguir comprando!</button></Link> </h2> ) 
+                            
+                            :(cartProducts.map((prod)=>{
+                            return(
+                            <div className='col-md-4'>
+                                    <Card style={{ width: '15rem'}}>
+                                        <Card.Img className='CartImg' variant="top" src={prod.Img} style={{ height: '300px'}} />
+                                        <Card.Body>
+                                            <Card.Title>{prod.Producto}</Card.Title>
+                                        </Card.Body>
+                                        <ListGroup className="list-group-flush">
+                                            <ListGroupItem>{prod.quantity}</ListGroupItem>
+                                            <ListGroupItem>Precio por unidad: ${prod.Precio}</ListGroupItem>
+                                            <ListGroupItem>Precio por cantidad: ${prod.quantity * prod.Precio}</ListGroupItem>
+                                        </ListGroup>
+                                    
+                                        <button className='btn btn-secondary' onClick={() =>removeItems(prod)}>eliminar producto</button>
+                                    </Card>
+                                </div>)
+                            }))}
+                        </div>    
+                    </div>    
+                </div>
+                  
+                <div className='col-md-4'>  
+                    <div className='conatainer'> 
+                        <div className='row'> 
+                            <div className='col-md-12'>      
+                                {cartProducts.length>0 &&        
+                                    (<div className='cartButtonController row '>
+                                        <div className='col-md-4'>
+                                            <div className='container'>
+                                                <div className='column'>
 
-
-                    }))}
+                                                    <div>
+                                                        <form onSubmit={sendOrder} className='container'>
+                                                            <div className=' formStyles row '>
+                                                                <input className='form-control mt-2' type='text' placeholder='Nombre'/>
+                                                                <input className='form-control mt-2' type='email' placeholder='Email'/>
+                                                                <input  className='form-control mt-2' type='number' placeholder='Telefono'/>
+                                                                <button className='btn btn-secondary mt-2'  type='submit'>enviar</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                        <br></br>
+                                                        <div className='conatainer'>
+                                                            <div className='button-flex'>
+                                                                <div className=''>
+                                                                    <button className=' clearCartButton btn btn-danger' onClick={clearCart}>vaciar carrito</button> 
+                                                                </div> 
+                                                                <div className='' >  
+                                                                    <div className='btn totalPricebutton btn-warning'>Precio Total: ${finalPrice}</div>
+                                                                </div>
+                                                                <div className=''>
+                                                                    <Link to={"/"}>
+                                                                        <button className='btn btn-success letBuyButton '>Seguir comprando!</button>
+                                                                    </Link>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                </div>            
+                                            </div>  
+                                        </div>
+                                    </div>
+                                    )
+                                }
+                            </div>
+                        </div>    
+                    </div>           
+                </div>
             </div>
-            {cartProducts.length>0 &&        
-            (  <div className='cartButtonController'>
-                <button className=' clearCartButton btn btn-danger' onClick={clearCart}>vaciar carrito</button>    
-                <div className='btn btn-warning'>Precio Total: ${finalPrice}</div>
-                <Link to={"/"}>
-                    <button className='btn btn-success'>Seguir comprando!</button>
-                </Link>
-                <form onSubmit={sendOrder}>
-                    <input type='text' placeholder='Nombre'/>
-                    <input type='email' placeholder='Email'/>
-                    <input type='number' placeholder='Telefono'/>
-                    <button type='submit'>enviar</button>
-                </form>
-            </div>)
-        }    
+            
         </div>
        
         )}
